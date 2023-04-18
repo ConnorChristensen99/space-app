c15d6bdce7c110ed340dd4a2660a551e2207f474
Connor Christensen <connor.christensen@uvu.edu>
2023-04-18 09:30:32 -0600

    Finished adding Mars Rover Photos and Worked on Adding NASA elements to App

--- src/pages/mars.js
+++ src/pages/mars.js
@@ -1,6 +1,10 @@
-
 import React, { useState, useEffect } from "react";
 
+function Mars() {
+const [mars1, setMARS1] = useState("")
+const [mars2, setMARS2] = useState("")
+const [mars3, setMARS3] = useState("")
+const [mars4, setMARS4] = useState("")
 
 const API_KEY = 'ubtDeq16oc6DaL16ddmYvUlEiRF5zGwnYRiYv7tn'
 
@@ -17,13 +21,15 @@ async function getPictures() {
  
   let pictures = await response.json()
 
- console.log(pictures)
+ setMARS1(pictures.photos[0].img_src)
+ setMARS2(pictures.photos[1].img_src)
+ setMARS3(pictures.photos[2].img_src)
+ setMARS4(pictures.photos[3].img_src)
 }
-
 getForecast()
 getPictures()
   
-const Mars = () => {
+
   return (
     <div>
       <h1>
@@ -33,8 +39,13 @@ const Mars = () => {
       <h3>
         Mars Rover Photos
       </h3>
+      <div id="marsPhotoss">
+      <img class="marsPhoto" src = {mars1}/>
+      <img class="marsPhoto" src = {mars2}/>
+      <img class="marsPhoto" src = {mars3}/>
+      <img class="marsPhoto" src = {mars4}/>
+      </div>
     </div>
   );
-};
-  
+}
 export default Mars;
--- src/pages/nasa.js
+++ src/pages/nasa.js
@@ -1,13 +1,65 @@
-import React from "react";
-  
-const NASA = () => {
+import React, { useState, useEffect } from "react";
+
+function Nasa() {
+const [engine1, setENGINE1] = useState("")
+const [engine2, setENGINE2] = useState("")
+const [engine3, setENGINE3] = useState("")
+
+const [software1, setSOFTWARE1] = useState("")
+const [software2, setSOFTWARE2] = useState("")
+const [software3, setSOFTWARE3] = useState("")
+
+
+
+const API_KEY = 'ubtDeq16oc6DaL16ddmYvUlEiRF5zGwnYRiYv7tn'
+
+async function getSoftware() {
+  const engine = await fetch(`https://api.nasa.gov/techtransfer/patent/?engine&api_key=${API_KEY}`)
+ 
+  let engines = await engine.json()
+
+ console.log(engines.results)
+  setENGINE1(engines.results[1][10])
+  setENGINE2(engines.results[5][10])
+  setENGINE3(engines.results[23][10])
+
+
+
+ const software = await fetch(`https://api.nasa.gov/techtransfer/software/?propulsion&api_key=${API_KEY}`)
+ 
+ let softwares = await software.json()
+
+console.log(softwares.results)
+  setSOFTWARE1(softwares.results[14][2])
+  setSOFTWARE2(softwares.results[15][2])
+  setSOFTWARE3(softwares.results[16][2])
+}
+
+
+
+getSoftware()
+
+
+
   return (
     <div>
       <h1>
-        NASA
+        Engines and Parts used by NASA
       </h1>
+      <div id="engine">
+      <img class="enginePhoto" src={engine1} />
+      <img class="enginePhoto" src={engine2} />
+      <img class="enginePhoto" src={engine3} />
+      </div>
+      <h3>
+       Propulsion Software Used by NASA
+      </h3>
+      <ul>
+        <li>{software1}</li>
+        <li>{software2}</li>
+        <li>{software3}</li>
+      </ul>
     </div>
   );
-};
-  
-export default NASA;
+}
+export default Nasa;
