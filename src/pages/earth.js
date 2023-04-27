30173539f1af2a8cf7fc97978db6c36c84144236
Connor Christensen <connor.christensen@uvu.edu>
2023-04-27 08:34:29 -0600

    Added ability for user to search images via lat and lon

--- src/pages/earth.js
+++ src/pages/earth.js
@@ -6,20 +6,31 @@ function Earth() {
 
   const [formLat, setFormLat] = useState(40.2969)
   const [formLon, setFormLong] = useState(111.6946)
+
+
+
   
-  async function getData() {
-    const response = await fetch(`https://api.nasa.gov/planetary/earth/imagery?lon=${formLon}&lat=${formLat}&date=2014-02-01&api_key=${API_KEY}`)
+  
+  useEffect(() => {
+    const fetchData = async () => {
+    const response = await fetch(`https://api.nasa.gov/EPIC/api/natural/images?api_key=${API_KEY}`)
    
-    let data = await response.json()
+    let earthImage = await response.json()
   
-   console.log(data)
+   console.log(earthImage) //I MIGHT NEED TO DO WHAT I DID BELOW IF IT IS RETURNING AN IMAGE
   }
+  fetchData()
+  }, [])
+
+
+
+
 
   function getImage(formLat, formLon) {
     const imageURL = `https://api.nasa.gov/planetary/earth/imagery?lon=${formLon}&lat=${formLat}&date=2014-02-01&api_key=${API_KEY}`
 
     return(
-      <img id="latImage" src={imageURL} alt="image of entered location" />
+      <img id="latImage" src={imageURL} alt="image of entered location"/>
     )
   }
 
@@ -42,6 +53,9 @@ function Earth() {
       <h1>
         EARTH
       </h1>
+
+      {/* EARTH IMAGE HERE */}
+
       <h4>Find a satellite Image!</h4>
       <form onSubmit={handleSubmit}>
       <TextField id="filled-basic" label="Enter Latitude" variant="filled" type="text" name="lat"/>
@@ -49,7 +63,7 @@ function Earth() {
       <input type="submit" value="Find Image" id="submitLong" />
       </form>
       <div id="satelliteImage">
-        {getImage()}
+        {getImage(formLat, formLon)}
       </div>
     </div>
   );
